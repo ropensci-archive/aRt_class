fb5fe4f15179a9c63e54715ccd5b8702bd9ecec4
Danielle Navarro <d.navarro@unsw.edu.au>
2019-12-13 15:15:38 +1100

    updated delaunay plot

--- plots/delaunay_image1.R
+++ plots/delaunay_image1.R
@@ -1,19 +1,25 @@
 library(jasmines)
 library(dplyr)
+library(ggplot2)
 source(here::here("palettes.R"))
+source(here::here("seed_delaunay.R"))
 
-set.seed(43)
-seed_delaunay(n = 20, grain = 50) %>%
+set.seed(4)
+dat <- seed_delaunay(n = 20, grain = 1000) %>%
+	filter(id %in% sample(max(id), ceiling(max(id)*.4))) %>%
 	mutate(x = x * 5, y = y * 5) %>%
   time_tempest(iterations = 100, curl_seed = 43, scale = .0075) %>%
-  mutate(order = time) %>%
+  mutate(order = time) 
+
+pic <- dat %>%
   style_ribbon(
   	palette = palette_whitened,
     alpha_decay = .03, 
   	alpha_init = 1, 
   	background = "white",
-  	seed_fill = "#aaaaaa44", 
-  	seed_col = "#000000"
+  	seed_fill = "#8BD3DA66", 
+  	seed_col = "#ffffffaa",
+  	seed_linewidth = 2
   ) %>%
-  export_image(here::here("output", "delaunay1.png"))
-
+	export_image(here::here("output", "delaunay1.png"))
+	
